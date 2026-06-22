1f43336259f5d2e70508649977a3c3ba5dbef63a
Ankita Mardana <119393582+AnkitaCodder@users.noreply.github.com>
2026-06-22 00:48:32 -0700

    Update SecurityConfig.java

--- backend/src/main/java/com/cognivaa/AMS/config/SecurityConfig.java
+++ backend/src/main/java/com/cognivaa/AMS/config/SecurityConfig.java
@@ -116,7 +116,8 @@ public class SecurityConfig {
 
         // ✅ Explicit frontend origin
         configuration.setAllowedOrigins(
-                List.of("http://192.168.56.10:5173",
+                List.of(
+                    // "http://192.168.56.10:5173",
                         "http://localhost:3000"
                         
                        )
